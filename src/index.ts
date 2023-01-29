f97a3889ae498cafca84b08fdd203e595c88205f
Sora Morimoto <sora@morimoto.io>
2023-01-30 02:16:18 +0900

    Update npm deps
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ const destinationURL = "https://twitter.com/east9698";
 const statusCode = 301;
 
 export default {
-  fetch(): Response {
+  fetch() {
     return Response.redirect(destinationURL, statusCode);
   },
 };
